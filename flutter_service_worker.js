21336f935e7478e6b0cdf642becc9fca228d29b8
Amin BADH <aminbadh@gmail.com>
2024-06-25 11:10:03 +0100

    Add .gitignore

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -38,8 +38,8 @@ const RESOURCES = {
 "icons/ms-icon-150x150.png": "cafbcd9f1025a35b94bd01b4a4b0e011",
 "icons/ms-icon-310x310.png": "ba091866088133846647cc63dbde6c27",
 "icons/ms-icon-70x70.png": "c0b46bf64749c30177930515a8887398",
-"index.html": "4918673a6643dc2f6c728b4183fee166",
-"/": "4918673a6643dc2f6c728b4183fee166",
+"index.html": "319fdeeedea7bf1e24c63b90870b5e09",
+"/": "319fdeeedea7bf1e24c63b90870b5e09",
 "main.dart.js": "c79dc73b49225aaeefe74539a179d718",
 "manifest.json": "2fb13076d67a71f2d79fb1612c2854c0",
 "version.json": "44ee7099d83581bb4a53c9070d41e157"
